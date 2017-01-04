4cc4e8812e54b9218a63e6df3e322728b00dbadc
Howard X <hxiao+scm@dargasea.com>
2017-01-04 10:17:02 +0800

    Move fetch_monitors to worker_tasks
    Part of the effort to reduce LoC outside the actual package.

--- gefion/worker_tasks.py
+++ gefion/worker_tasks.py
@@ -3,6 +3,7 @@
 
 import json
 import logging
+from datetime import datetime
 from urllib.parse import urljoin
 
 import requests
@@ -80,3 +81,36 @@ def run_monitor(monitor_id, unique_id, check_name, arguments, endpoint_url):
         return True
 
     return False
+
+
+def fetch_monitors(scheduler, config):
+    """Fetch Monitors and schedule accordingly.
+
+    Arguments:
+        scheduler (rq_scheduler.Scheduler): The Schedule instance initialized.
+        config (dict): Entire loaded config.
+    """
+    monitors_url = urljoin(config['master'].get('endpoint'), 'monitors')
+    logging.info('Requesting endpoint for monitors at %s.', monitors_url)
+    r = requests.get(monitors_url,
+                     auth=(config.get('my_name'), config['master'].get('key')))
+    logging.debug('Master returned following Monitors: %s.', r.text)
+    monitors = json.loads(r.text).get('monitors')
+
+    for job in scheduler.get_jobs():  # Delete all existing jobs.
+        scheduler.cancel(job)
+
+    # scheduler.enqueue_in(timedelta(minutes=10), fetch_monitors, scheduler)
+    for monitor in monitors:
+        scheduler.schedule(
+            scheduled_time=datetime.utcnow(),
+            func=run_monitor,
+            args=[
+                monitor['id'],
+                monitor['unique_id'],
+                monitor['check'],
+                json.loads(monitor['arguments']),
+                config['master'].get('endpoint')],
+            interval=monitor['frequency'] * 60,  # Minutes to seconds.
+            repeat=None  # Repeat forever (until deletion).
+        )
--- run_worker.py
+++ run_worker.py
@@ -2,17 +2,13 @@
 """Worker task executions."""
 
 import argparse
-import json
 import logging
-from datetime import datetime
-from urllib.parse import urljoin
 
-import requests
 import yaml
 from redis import Redis
 from rq_scheduler import Scheduler
 
-from gefion import worker_tasks
+from gefion.worker_tasks import fetch_monitors
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -34,37 +30,5 @@ redis_port = int(config.get('rq', dict()).get('port', 6379))
 scheduler = Scheduler(connection=Redis(host=redis_host, port=redis_port))
 
 
-def fetch_monitors(scheduler):
-    """Fetch Monitors and schedule accordingly.
-
-    Arguments:
-        scheduler (rq_scheduler.Scheduler): The Schedule instance initialized.
-    """
-    monitors_url = urljoin(config['master'].get('endpoint'), 'monitors')
-    logging.info('Requesting endpoint for monitors at %s.', monitors_url)
-    r = requests.get(monitors_url,
-                     auth=(config.get('my_name'), config['master'].get('key')))
-    logging.debug('Master returned following Monitors: %s.', r.text)
-    monitors = json.loads(r.text).get('monitors')
-
-    for job in scheduler.get_jobs():  # Delete all existing jobs.
-        scheduler.cancel(job)
-
-    # scheduler.enqueue_in(timedelta(minutes=10), fetch_monitors, scheduler)
-    for monitor in monitors:
-        scheduler.schedule(
-            scheduled_time=datetime.utcnow(),
-            func=worker_tasks.run_monitor,
-            args=[
-                monitor['id'],
-                monitor['unique_id'],
-                monitor['check'],
-                json.loads(monitor['arguments']),
-                config['master'].get('endpoint')],
-            interval=monitor['frequency'] * 60,  # Minutes to seconds.
-            repeat=None  # Repeat forever (until deletion).
-        )
-
-
 if __name__ == '__main__':
-    fetch_monitors(scheduler)
+    fetch_monitors(scheduler, config)
